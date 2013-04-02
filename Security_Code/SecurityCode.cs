c37f1e74a748494b1e01dc559494266daeb88d1b
LiaoHaojie <liaohaojie@126.com>
2013-04-02 21:17:01 +0800

    RNGCryptoServiceProvider
    use RNGCryptoServiceProvider  to generate Random seed

--- Security_Code/SecurityCode.cs
+++ Security_Code/SecurityCode.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.Collections;
+using System.Security.Cryptography;
 
 namespace Security_Code
 {
@@ -14,33 +15,51 @@ namespace Security_Code
             Stopwatch timer1 = new Stopwatch();//计时器类
             timer1.Start();//开始计时
             SecurityCode c = new SecurityCode();
-            int ilen = Convert.ToInt32(args[0]);
-            int icount = Convert.ToInt32(args[1]);
+            int ilen = Convert.ToInt32(args[0]);//第一个参数
+            int icount = Convert.ToInt32(args[1]);//第二个参数
             c.createSecurityCode(ilen, icount);
             timer1.Stop();//停止计时
             double dMilliseconds = timer1.Elapsed.TotalMilliseconds;
             Console.WriteLine("生成个数为：{0}，运行时间为：{1}", icount, dMilliseconds);
             Console.ReadKey();
         }
-
+        /// <summary>
+        /// 生成随机安全码
+        /// </summary>
+        /// <param name="len">安全码长度</param>
+        /// <param name="count">安全码个数</param>
         public void createSecurityCode(int len, int count) 
         {
             string strTableChar = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ";
-            StringBuilder codeStr = new StringBuilder(10);
-            List<string> list = new List<string>();
-            Random ro = new Random();
+            StringBuilder codeStr = new StringBuilder(len);
+            Random ro = new Random(GetRandomSeed());
             int iResult;
-            int iUp = 34;
+            int iup = strTableChar.Length;
+            if (len < 1 || count < 1)
+            {
+                Console.WriteLine("输入值需为大于0的整数");
+            }
             for (int i = count; i > 0; i--)
             {
                 for (int j = len; j > 0; j--)
                 {
-                    iResult = ro.Next(iUp);
-                    codeStr.Append(strTableChar[iResult]);
+                    iResult = ro.Next(iup);
+                    codeStr.Append(strTableChar.Substring(iResult, 1));
                 }
-                Console.WriteLine(codeStr);
+                //Console.WriteLine(codeStr);
                 codeStr.Clear();
             }
+        }
+        /// <summary>
+        /// System.Security.Cryptography.RNGCryptoServiceProvider 生成 Random 种子
+        /// </summary>
+        /// <returns></returns>
+        static int GetRandomSeed()
+        {
+            byte[] bytes = new byte[4];
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            return BitConverter.ToInt32(bytes, 0);
         }
     }
 }
