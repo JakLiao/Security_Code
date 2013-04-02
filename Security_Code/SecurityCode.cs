31a3024900fd93ebc2a74ae13554cb43262ffcec
廖浩杰 <1484725248@qq.com>
2013-04-02 15:29:04 +0800

    first edition

---
+++ Security_Code/SecurityCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Collections;
+
+namespace Security_Code
+{
+    class SecurityCode
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer1 = new Stopwatch();//计时器类
+            timer1.Start();//开始计时
+            SecurityCode c = new SecurityCode();
+            int ilen = Convert.ToInt32(args[0]);
+            int icount = Convert.ToInt32(args[1]);
+            c.createSecurityCode(ilen, icount);
+            timer1.Stop();//停止计时
+            double dMilliseconds = timer1.Elapsed.TotalMilliseconds;
+            Console.WriteLine("生成个数为：{0}，运行时间为：{1}", icount, dMilliseconds);
+            Console.ReadKey();
+        }
+
+        public void createSecurityCode(int len, int count) 
+        {
+            string strTableChar = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ";
+            Hashtable hashtable = new Hashtable(); 
+            for (int i = count; i > 0; i--)
+            {
+                Random ro = new Random();
+                int iResult;
+                int iUp = 34;
+                iResult = ro.Next(iUp);
+                Console.WriteLine(iResult);
+            }
+        }
+    }
+}
